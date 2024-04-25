58110a5955e9b5e3b5bc9532c689026f22c35aa3
dkflfkd53 <b57062562@gmail.com>
2024-04-25 16:15:24 +0900

    first commit

---
+++ src/test/java/com/example/public_server/PublicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.public_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
